cfa6bacf14f1122428b7596ccd1b5cfe107b077b
lunjingjie <lunjj@create80.com>
2018-01-12 13:58:53 +0800

    添加测试方法

--- src/test/java/TestX.java
+++ src/test/java/TestX.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import javax.xml.transform.Source;
+import java.util.*;
+
 public class TestX {
     @Test
     public void testXxX() {
@@ -25,6 +28,46 @@ public class TestX {
                     array[j] = temp;
                 }
             }
+        }
+    }
+
+    /**
+     * 测试集合
+     */
+    @Test
+    public void testIterator() {
+        Map map = new HashMap();
+        map.put("test1", "value1");
+        map.put("test4", "value2");
+        map.put("test1", "value3");
+        Set entryset = map.entrySet();
+        Iterator it = entryset.iterator();
+        while (it.hasNext()) {
+            Map.Entry me = (Map.Entry) it.next();
+            Object key = me.getKey();
+            Object value = me.getValue();
+            System.out.println(key + ":" + value);
+        }
+    }
+
+    /**
+     * 测试Collections.sort();
+     */
+    @Test
+    public void testSort() {
+        List aList = new ArrayList();
+        aList.add("abc");
+        aList.add("agcs");
+        aList.add("ss");
+        aList.add("afss");
+        Collections.sort(aList, new Comparator<String>() {
+            public int compare(String o1, String o2) {
+                int tmp = o1.length() - o2.length();
+                return tmp == 0 ? o1.compareTo(o2) : tmp;
+            }
+        });
+        for (Object a : aList) {
+            System.out.println(a);
         }
     }
 }
